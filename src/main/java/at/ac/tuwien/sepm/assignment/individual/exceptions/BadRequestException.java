8facc5adc45f597bac327cdb083ddc2e62575afd
nikita <e1526589@student.tuwien.ac.at>
2019-03-20 09:47:14 +0100

    Commit test

--- src/main/java/at/ac/tuwien/sepm/assignment/individual/exceptions/BadRequestException.java
+++ src/main/java/at/ac/tuwien/sepm/assignment/individual/exceptions/BadRequestException.java
@@ -1,7 +1,11 @@
 package at.ac.tuwien.sepm.assignment.individual.exceptions;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 
 public class BadRequestException extends Exception {
+    private static final Logger LOGGER = LoggerFactory.getLogger(BadRequestException.class);
     public BadRequestException(String msg){
         super(msg);
+        LOGGER.error("Error: " + msg);
     }
 }
